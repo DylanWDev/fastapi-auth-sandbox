93ea949bbf0d365f7f15ad6636ace2f6f75cd6b3
Dylan Wyrick <xpoweroutage@gmail.com>
2023-11-13 16:08:57 +0000

    ok

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from database import engine
+from sqlalchemy import select
+from fastapi.response import PlainTextResponse
+from starlette.exception import HTTPException as StarletteHTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import models
 
 app = FastAPI()
 
+# origins = [
+#     ""
+# ]
 
 @app.get("/")
 async def root():
